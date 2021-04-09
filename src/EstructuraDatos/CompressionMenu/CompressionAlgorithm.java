e04ed49c88ed5f72410d6cfa337ee280c5206ae4
PipesSex <pipesjimenez6@gmail.com>
2021-04-09 14:34:13 -0500

    arreglando package ya quedo bien

--- src/EstructuraDatos/CompressionMenu/CompressionAlgorithm.java
+++ src/EstructuraDatos/CompressionMenu/CompressionAlgorithm.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package compresionmenu;
+package EstructuraDatos.CompressionMenu;
 
 //Creación de la interfaz con el método compress que recibe el paramétro filename de tipo String
 interface CompressionAlgorithm {
