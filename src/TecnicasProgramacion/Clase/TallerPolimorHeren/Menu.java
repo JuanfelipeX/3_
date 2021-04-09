552c705f9d91fe2a6b2ce85a7faa7ae3ead840c4
PipesSex <pipesjimenez6@gmail.com>
2021-04-09 14:42:27 -0500

    Agregando los Joption para entrada de datos

--- src/TecnicasProgramacion/Clase/TallerPolimorHeren/Menu.java
+++ src/TecnicasProgramacion/Clase/TallerPolimorHeren/Menu.java
@@ -1,14 +1,21 @@
 package TecnicasProgramacion.Clase.TallerPolimorHeren;
 
+import javax.swing.JOptionPane;
+
 public class Menu {
 
     public static void main(String[] args) {
         
         try {
 
-            Profesor profesor = new Profesor(0, null);
-            Estudiante estudiante = new Estudiante(0, null);
-        
+            int idEstudiante = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el id del Estudiante "));
+            String nameEstudiante = (JOptionPane.showInputDialog("Ingrese el nombre del estudiante "));
+            Estudiante estudiante = new Estudiante(idEstudiante, nameEstudiante);
+
+            int idProfe = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el id del Profesor "));
+            String nameProfe = (JOptionPane.showInputDialog("Ingrese el nombre del Profesor "));
+            Profesor profesor = new Profesor(idProfe, nameProfe);
+
         } catch (Exception e) {
             System.out.println("Error " + e);
         }
