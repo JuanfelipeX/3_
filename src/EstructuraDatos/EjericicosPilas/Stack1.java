3129539c495f52a223c6cfa62e5938bfa5a4ac7e
pipeSex <pipesjimenez6@gmail.com>
2021-05-03 17:17:07 -0500

    Ejercicio completado de pilas Stack

--- src/EstructuraDatos/EjericicosPilas/Stack1.java
+++ src/EstructuraDatos/EjericicosPilas/Stack1.java
@@ -1,4 +1,4 @@
-package EstructuraDatos.EjericicosPilas;
+//package EstructuraDatos.EjericicosPilas;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,40 +12,45 @@ class Stack1 {
     
     public static void main(String[] args) throws NumberFormatException, IOException {
 
-        try {
+        int N = Integer.parseInt(br.readLine());
 
-            int N = Integer.parseInt(br.readLine());
+        Stack<Integer> pila = new Stack<Integer>();
 
-            Stack<Integer> pila = new Stack<Integer>();
+        boolean error = false;
 
-            for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
 
-                String textos = br.readLine();
-                String[] parts = textos.split(" ");
-                String signo = parts[0];
+            String textos = br.readLine();
+            String[] parts = textos.split(" ");
+            String signo = parts[0];
 
-                if (signo.equals("-")) {
-                    Integer objetoBorrado = null;
+            if (signo.equals("-")) {
+                Integer objetoBorrado = null;
+                
+                if (!pila.isEmpty()) {
+                    
                     objetoBorrado = pila.pop();
-
                     System.out.println(objetoBorrado);
 
                 } else {
-                    int x = Integer.parseInt(parts[1]);
-                    pila.push(x);
-
-                }
-                if (pila.empty()) {
                     System.out.println("empty");
-
                 }
+                
+                
+                
+                error = true;
+                
+            } else {
+                int x = Integer.parseInt(parts[1]);
+                pila.push(x);
+                
             }
+            
+        }
 
-        } catch (EmptyStackException e) {
-
+        while (!error) {
             System.out.println("empty");
         }
     }
-        
 }
 
